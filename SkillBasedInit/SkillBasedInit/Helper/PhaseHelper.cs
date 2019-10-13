f033c10fec78d8f1978023921e44d3e3f009e75a
Gerrit Kitts <gkitts@gmail.com>
2019-10-13 15:23:24 -0400

    Moving files

---
+++ SkillBasedInit/SkillBasedInit/Helper/PhaseHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBasedInit {
+    public static class PhaseHelper {
+
+        // Calculate the left and right phase boundaries *as initiative* 
+        //   Will calculate 
+        // 30, 29, 28, 27, 26 (red 30)
+        // 30, 29, 28, 27, 26 (red 29)
+        // 30, 29, 28, 27, 26 (red 28)
+        // 29, 28, 27, 26, 25 (red 27)
+        // 28, 27, 26, 25, 24 (red 26)
+        // ...
+        //  7,  6,  5,  4,  3 (red 5)
+        //  6,  5,  4,  3,  2 (red 4)
+        //  5,  4,  3,  2,  1 (red 3)
+        //  5,  4,  3,  2,  1 (red 2)
+        //  5,  4,  3,  2,  1 (red 1)
+        public static int[] CalcPhaseIconBounds(int currentPhase) {
+
+            // Normalize phase to initiative values
+            int currentInit = (Mod.MaxPhase + 1) - currentPhase;
+            
+            int leftPhase = currentInit;
+            if (currentInit + 2 > Mod.MaxPhase) {
+                leftPhase = Mod.MaxPhase;
+            } else if (currentInit - 2 < Mod.MinPhase) {
+                leftPhase = Mod.MinPhase + 4;
+            }
+
+            int[] bounds = new int[] { leftPhase, leftPhase -4 };
+            Mod.Log.Info($"For phase {currentPhase}, init bounds are: {bounds[0]} to {bounds[1]}");
+            return bounds;
+        }
+    }
+}
---
+++ SkillBasedInit/SkillBasedInit/patches/UI/HUDPhaseTrack.cs
@@ -0,0 +1,113 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace SkillBasedInit {
+
+    // ========== CombatHUDPhaseTrack ==========
+    [HarmonyPatch(typeof(CombatHUDPhaseTrack), "Init")]
+    [HarmonyPatch(new Type[] { typeof(CombatGameState), typeof(CombatHUD) })]
+    public static class CombatHUDPhaseTrack_Init {
+
+        public static void Postfix(CombatHUDPhaseTrack __instance, CombatGameState combat, CombatHUD HUD, 
+            List<CombatHUDPhaseIcons> ___PhaseIcons, CombatHUDReserveButton ___reserveButton) {
+
+            Mod.Log.Info($"CHUDPT:I:post - Init");
+
+            __instance.OnCombatGameDestroyed();
+
+            combat.MessageCenter.Subscribe(MessageCenterMessageType.OnRoundBegin, new ReceiveMessageCenterMessage(__instance.OnRoundBegin), true);
+            combat.MessageCenter.Subscribe(MessageCenterMessageType.OnPhaseBegin, new ReceiveMessageCenterMessage(__instance.OnPhaseBegin), true);
+
+            ___reserveButton.DisableButton();
+
+            __instance.Visible = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatHUDPhaseTrack), "OnRoundBegin")]
+    [HarmonyPatch(new Type[] { typeof(MessageCenterMessage) })]
+    public static class CombatHUDPhaseTrack_OnRoundBegin {
+        public static void Postfix(CombatHUDPhaseTrack __instance, MessageCenterMessage message,
+            TextMeshProUGUI ___roundCounterText, List<CombatHUDIconTracker> ___IconTrackers,
+            GameObject ___phaseTrack, GameObject[] ___phaseBarHolders) {
+
+            Mod.Log.Info($"CHUDPT:ORB:post - Init");
+            for (int i = 0; i < ___IconTrackers.Count; i++) {
+                //___IconTrackers[i].Visible = false;
+            }
+           // ___phaseTrack.SetActive(false);
+
+            for (int i = 0; i < ___phaseBarHolders.Length; i++) {
+                //___phaseBarHolders[i].SetActive(false);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatHUDPhaseTrack), "OnPhaseBegin")]
+    [HarmonyPatch(new Type[] { typeof(MessageCenterMessage) })]
+    public static class CombatHUDPhaseTrack_OnPhaseBegin {
+        public static void Postfix(CombatHUDPhaseTrack __instance, MessageCenterMessage message, TextMeshProUGUI ___roundCounterText,
+            CombatHUDPhaseBar[] ___phaseBars, int ___currentPhase) {
+
+            Mod.Log.Info($"CHUDPT:OPB:post - Init");
+            PhaseBeginMessage phaseBeginMessage = message as PhaseBeginMessage;
+            string phaseText = string.Format("{0} - Phase {1}", phaseBeginMessage.Round, 31 - phaseBeginMessage.Phase);
+            ___roundCounterText.SetText(phaseText);
+
+            CombatHUDPhaseTrack_SetPhaseTexts.UpdatePhaseTexts(___phaseBars, ___currentPhase);
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatHUDPhaseTrack), "SetPhaseTexts")]
+    public static class CombatHUDPhaseTrack_SetPhaseTexts {
+        public static bool Prefix(CombatHUDPhaseTrack __instance, CombatHUDPhaseBar[] ___phaseBars, int ___currentPhase) {
+
+            UpdatePhaseTexts(___phaseBars, ___currentPhase);
+            return false;
+        }
+
+        public static void UpdatePhaseTexts(CombatHUDPhaseBar[] phaseBars, int currentPhase) {
+
+            int[] bounds = PhaseHelper.CalcPhaseIconBounds(currentPhase);
+            for (int i = 0; i < 5; i++) {
+                phaseBars[i].Text.SetText(AbstractActor.InitiativeToString(bounds[0] - i), Array.Empty<object>());
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatHUDPhaseTrack), "SetTrackerPhase")]
+    [HarmonyPatch(new Type[] { typeof(CombatHUDIconTracker), typeof(int) })]
+    public static class CombatHUDPhaseTrack_SetTrackerPhase {
+        public static bool Prefix(CombatHUDPhaseTrack __instance, CombatHUDIconTracker tracker, int phase, List<CombatHUDPhaseIcons> ___PhaseIcons) {
+            int[] bounds = PhaseHelper.CalcPhaseIconBounds(phase);
+            int phaseAsInit = (Mod.MaxPhase + 1) - phase;
+            Mod.Log.Info($"Phase {phase} is init {phaseAsInit} with bounds: {bounds[0]}-{bounds[1]}");
+
+            if (phaseAsInit > bounds[0] || phaseAsInit < bounds[1]) {
+                Mod.Log.Info($" Phase outside of current bounds, skipping.");
+            } else {
+                int iconIdx = bounds[0] - phaseAsInit;
+                Mod.Log.Info($"Phase inside bounds, icon index is: {iconIdx}");
+                ___PhaseIcons[phase - 1].AddIconTrackerToPhase(tracker);
+            }
+
+            //tracker.CurrentlyDisplayedPhase = phase;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(CombatHUDIconTracker), "RefreshColor")]
+    public static class CombatHUDIconTracker_RefreshColor_Prefix {
+        public static bool Prefix(CombatHUDIconTracker __instance) {
+            Mod.Log.Debug("CHUDIT:RC entered.");
+
+            return false;
+        }
+    }
+
+}
