de31ff0e446ade73870d6d345af8d1d4e1f55cce
IceRaptor <gkitts@gmail.com>
2023-04-16 08:09:33 -0400

    - Convert to MS 2019 project style

--- SkillBasedInit/SBITests/Properties/AssemblyInfo.cs
+++ SkillBasedInit/SBITests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("SBITests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SBITests")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("cd7c7dab-5fca-42b9-8c13-99fdb99e2e09")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
